f495c8b73eedce2e0dcd34424f4523d94f4505c7
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-04-17 12:19:24 +0530

    Add files via upload

--- Project_210/src/mq/Dropdown/DropdownSelection_Using_ClickMethod.java
+++ Project_210/src/mq/Dropdown/DropdownSelection_Using_ClickMethod.java
@@ -5,19 +5,31 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class DropdownSelection_Using_ClickMethod {
+public class DropdownSelection_Using_ClickMethod 
+{
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	{
 		
 		WebDriver driver=new ChromeDriver();
 		driver.get("https://facebook.com/reg");
 		driver.manage().window().maximize();
 		
-		//Identifying Child element under parent element
+		/*
+		 * => Identifying Parent Object   [Select Tag Element]
+		 * => Identifying Child Object under parent object  [Option Tag Element]
+		 * => Then we used click optiont select one of dropdown option
+		 */
+		
+		
 		WebElement DOB_Day=driver.findElement(By.id("day"));
 		DOB_Day.findElement(By.xpath("//option[@value='26'][contains(.,'26')]")).click();
 		
 		
+		/*
+		 * => I Copied direct xpath of Dropdown Option [Option tag xpath]
+		 * => Then I Located element and perform click action
+		 */
 		WebElement DOB_Month=driver.findElement(By.xpath("//option[@value='10'][contains(.,'Oct')]"));
 		DOB_Month.click();
 		
