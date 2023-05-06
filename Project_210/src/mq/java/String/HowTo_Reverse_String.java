59fbd68acc1fe3c2ebdfc5d1f3069f2cd74b5022
sunilreddyg <sunilreddy.gajjala@gmail.com>
2023-05-06 12:27:52 +0530

    Add files via upload

---
+++ Project_210/src/mq/java/String/HowTo_Reverse_String.java
@@ -0,0 +1,14 @@
+package mq.java.String;
+
+public class HowTo_Reverse_String {
+
+	public static void main(String[] args) {
+		
+		
+		StringBuilder builder=new StringBuilder("linus");
+		System.out.println(builder.reverse());
+		
+
+	}
+
+}
